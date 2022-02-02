da42122954e92d004f91a565a2c74ef62ac5771e
Toxirjon <tokhiriy87@gmail.com>
2022-02-02 16:41:14 +0500

    sssss

--- js/script.js
+++ js/script.js
@@ -1,1 +1,11 @@
-alert("Hello world");
+window.addEventListener("DOMContentLoaded", () =>{
+    const loader = document.querySelector(".loader");
+    setTimeout(() => {
+        loader.style.opacity = "0";
+        setTimeout(() => {
+            loader.style.display = "none";
+            
+        }, 1000);
+        
+    }, 1500);
+});
